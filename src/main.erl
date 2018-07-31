9a80c03b087122f89c7279d4845f0fc4760d312e
Alexandru Duma <alexandru.duma@spyhce.com>
2018-07-31 16:01:55 +0300

    Added rudimentary configuration file parsing.

---
+++ src/main.erl
@@ -0,0 +1,27 @@
+-module(main).
+
+-export([main/0]).
+
+process_lines(Device, Parser) ->
+    process_lines_i(Device, Parser, []).
+
+process_lines_i(Device, Parser, List) ->
+    case io:get_line(Device, "") of
+        eof -> List;
+        Line -> Result = Parser(Line),
+                process_lines_i(Device, Parser, [Result] ++ List)
+    end.
+
+extract_data(Line) ->
+    [NodeName, RawNeighbours] = string:tokens(Line, " "),
+    NeighbourList = string:tokens(clean(RawNeighbours), ","),
+    {NodeName, NeighbourList}.
+
+clean(Text) ->
+    re:replace(Text, "(^\\s+)|(\\s+$)", "", [global,{return,list}]).
+
+main() ->
+    {ok, Device} = file:open("./res/net.top", [read]),
+    Data = process_lines(Device, fun(Line) -> extract_data(Line) end),
+    io:format("~p", [Data]).
+
