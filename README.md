1ed212bc0aedf4b41d05975c2f5461c0b2718cce
Alexandru Duma <alexandru.duma@spyhce.com>
2018-07-31 13:53:44 +0300

    Restructured as an OTP application for use with rebar3

---
+++ README.md
@@ -0,0 +1,9 @@
+erlang_routing
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
--- main.erl
+++
@@ -1,8 +0,0 @@
--module(main).
-
--export([main/0]).
-
-main() ->
-    Child = spawn(er_node, start, []),
-    Child ! {quit, {get_routes, []}}.
-
